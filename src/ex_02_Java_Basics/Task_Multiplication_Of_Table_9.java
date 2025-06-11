951cf8fa9731d3c15a7af2a7fed52c87afaf5d7a
chandudad <chandanamallepoola17@gmail.com>
2025-06-11 06:32:51 +0530

    11th June 2025

---
+++ src/ex_02_Java_Basics/Task_Multiplication_Of_Table_9.java
@@ -0,0 +1,40 @@
+package ex_02_Java_Basics;
+
+public class Task_Multiplication_Of_Table_9 {
+    public static void main(String[] args) {
+        System.out.println("Multiplication of 9 table");
+        int a = 9;
+        int b = 1;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+                b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+    }
+}
