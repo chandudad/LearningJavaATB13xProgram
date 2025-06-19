d6a234fc3cb500b720704457fdb4d09fa6036640
chandudad <chandanamallepoola17@gmail.com>
2025-06-19 09:22:57 +0530

    11th June 2025

---
+++ src/ex_Java_Task/Print_Numers_From_One_To_Ten_Using_For_Loop.java
@@ -0,0 +1,14 @@
+package ex_Java_Task;
+
+public class Print_Numers_From_One_To_Ten_Using_For_Loop {
+    public static void main(String[] args) {
+        System.out.println("value from 1 to 10");
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(i);
+        }
+
+
+    }
+
+}
