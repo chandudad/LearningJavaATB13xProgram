72ec91ce586b9d5ea785ce9c27e0e41412968585
chandudad <chandanamallepoola17@gmail.com>
2025-06-19 08:44:26 +0530

    11th June 2025

---
+++ src/ex_Java_Task/String_Concatenation_with_numbers.java
@@ -0,0 +1,14 @@
+package ex_Java_Task;
+
+public class String_Concatenation_with_numbers {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        System.out.println("concatinating value=" + a + b);
+        System.out.println("addition =" + (a + b));
+        System.out.println("Subtraction =" + (a - b));
+        System.out.println("Multiplication =" + (a * b));
+        System.out.println("Division =" + (a / b));
+        System.out.println("Remainder =" + (a % b));
+    }
+}
